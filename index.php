2b0b2b5b5d7baa2b4d5bbfa39444a5bb2a144459
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-11-09 18:57:27 +0200

    added table and css

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bakery</title>
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<table>
+	<thead>
+		
+
+	</thead>
+
+	<tbody>
+		
+		<tr>
+
+			<td>Aguonele</td>
+
+		</tr>
+
+		<tr>
+			
+			<td>Varskes</td>
+
+		</tr>
+
+		<tr>
+			
+			<td>Duona balta </td>
+
+		</tr>
+
+		<tr>
+			
+			<td>Duona juoda</td>
+
+		</tr>
+
+		<tr>
+			
+			<td>Duona juoda</td>
+
+		</tr>
+
+	</tbody>
+
+
+
+
+
+</body>
+</html>
