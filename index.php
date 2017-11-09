eea7eca319cf112e7e9f6cd20b029af3c0c75423
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-11-09 19:38:32 +0200

    added products

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title>bakery</title>
 	<link rel="stylesheet" href="css/app.css">
+
 </head>
 <body>
-<table>
-	<thead>
-		
+<table class="table">
+	<thead class="table-header">
+		<tr>
+			<th rowspan="2"> Pavadinimas</th>
+			<th colspan="5">2017-11-09 </th>
+			<th colspan="5">2017-11-10s</th>
+		</tr>
+		<tr>
+			<th>VL</th> <!--Vakarykstis likutis-->
+			<th>PG</th> <!--Pagaminta-->
+			<th>PR</th> <!--Parduota-->
+			<th>SG</th> <!--Sugadinta-->
+			<th>GL</th> <!--Galutinis likutis-->
+			<th>VL</th> <!--Vakarykstis likutis-->
+			<th>PG</th> <!--Pagaminta-->
+			<th>PR</th> <!--Parduota-->
+			<th>SG</th> <!--Sugadinta-->
+			<th>GL</th> <!--Galutinis likutis-->
+		</tr>
 
 	</thead>
 
@@ -16,30 +34,95 @@
 		<tr>
 
 			<td>Aguonele</td>
+			<td>5</td>
+			<td>26</td>
+			<td>13</td>
+			<td>3</td>
+			<td>10</td>
+			<td>5</td>
+			<td>26</td>
+			<td>13</td>
+			<td>3</td>
+			<td>10</td>
 
 		</tr>
 
 		<tr>
 			
 			<td>Varskes</td>
+			<td>6</td>
+			<td>26</td>
+			<td>13</td>
+			<td>1</td>
+			<td>18</td>
+			<td>6</td>
+			<td>26</td>
+			<td>13</td>
+			<td>1</td>
+			<td>18</td>
 
 		</tr>
 
 		<tr>
 			
 			<td>Duona balta </td>
+			<td>2</td>
+			<td>26</td>
+			<td>18</td>
+			<td>3</td>
+			<td>7</td>
+			<td>2</td>
+			<td>26</td>
+			<td>18</td>
+			<td>3</td>
+			<td>7</td>
 
 		</tr>
 
 		<tr>
 			
 			<td>Duona juoda</td>
-
+			<td>0</td>
+			<td>26</td>
+			<td>13</td>
+			<td>6</td>
+			<td>7</td>
+			<td>0</td>
+			<td>26</td>
+			<td>13</td>
+			<td>6</td>
+			<td>7</td>
 		</tr>
 
 		<tr>
 			
 			<td>Duona juoda</td>
+			<td>20</td>
+			<td>26</td>
+			<td>13</td>
+			<td>0</td>
+			<td>23</td>
+			<td>20</td>
+			<td>26</td>
+			<td>13</td>
+			<td>0</td>
+			<td>23</td>
+
+		</tr>
+
+		<tr>
+
+			<td>Duona rugine</td>
+			<td>1</td>
+			<td>30</td>
+			<td>10</td>
+			<td>1</td>
+			<td>20</td>
+			<td>1</td>
+			<td>30</td>
+			<td>10</td>
+			<td>1</td>
+			<td>20</td>
 
 		</tr>
 
