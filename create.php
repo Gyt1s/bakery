91f692d7a3fb41a9190c42230f8d8466006203a5
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-11-17 21:22:03 +0200

    object

--- create.php
+++ create.php
@@ -2,8 +2,40 @@
 
 $new_data = ($_POST);
 
+formatData ($new_data);
+
 var_dump ($new_data);
 
-$existing_data = file_get_contents('data/bakery-data.json');
+$existing_data = json_decode (file_get_contents('data/bakery-data.json'));
+//var_dump($existing_data);
 
+$existing_data = objectToArray ($existing_data);
 var_dump($existing_data);
+//var_dump( (array) ($existing_data);
+
+
+function formatOfData(array &$data)
+{
+
+}
+
+
+
+function objectToArray (stdClass $obj) : array
+{
+	$obj = (array) $obj;
+
+	foreach ($obj as $key => &$value) {
+
+		if( gettype ($value) == 'object')
+		{
+			$value = objectToArray ($value);
+		}
+
+	}
+
+
+	return $obj;
+}
+
+
