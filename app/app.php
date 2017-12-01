0bc93e4ddb5071dce0eb6cf80f293e2e8503abde
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-12-01 19:10:55 +0200

    added table again

--- app/app.php
+++ app/app.php
@@ -1,6 +1,27 @@
 <?php 
 
-$data = json_decode(file_get_contents('data/bakery-data.json'), true);
+@include_once('database.php');
+
+//GETTING PRODUCTS HISTORY
+$query = 'SELECT * FROM `bakery_products_history` ORDER BY `date`';
+
+$productHistory = db_query($query);
+
+foreach ($result as $key => $value)
+{
+	print_r($value);
+}
+
+//GET PRODUCTS
+$query = 'SELECT * FROM `bakery_products`';
+
+$products = db_query($query);
+
+foreach ($products as $key => $value)
+{
+	print_r($value);
+}
+//$data = json_decode(file_get_contents('data/bakery-data.json'), true);
 
 
 @include_once('view/data-table.php');
