a4d1219e9cd4b43f490e77dc0255479712aadeb6
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-12-01 20:29:07 +0200

    improvement table 2

--- app/view/data-table.php
+++ app/view/data-table.php
@@ -1,20 +1,39 @@
 <?php
 
-$days = $keys = '';
-$rows = [];
+$rows = $days = [];
+
+$keys = '';
 /*
 $products= json_decode (file_get_contents('data/products.json'), true);
 
 ksort($data);
 */
+	foreach ($products as $value) {	
+
+		if (!isset($rows[$value['id']]))
+		{
+			$rows[$value['id']] = '<td>' . $value['name'] . '</td>';
+		}
+
+	}
 
 	foreach ($productHistory as $value)
-	{		
+	{
+		/*print_r($value);
+		die();*/
 
+		if  (!isset($days[$value['date']]))
+		{
 
-		$days .= '<th colspan="5">' . $value ['date'] . '</th>';
+		$days[$value['date']] = $value ['date'];
 		$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
+/*
+		if (!isset($rows[$value['product_id']))
+			{
+				$rows[$value['product id']] .= '<td>' . $value['product_id'] . '</td>';
+			}*/
 
+		}
 		/*foreach ($products as $key => $name) {	
 
 				if (!isset($rows[$key]))
@@ -46,7 +65,10 @@ ksort($data);
 		<tr>
 			<th rowspan="2"> Pavadinimas</th>
 			<?php
-				echo $days;
+
+			foreach ($days as $date) {
+			echo '<th colspan="5">' . $date . '<th>';
+		}
 			?>
 		</tr>
 		<tr>
@@ -58,8 +80,8 @@ ksort($data);
 	<tbody>
 		
 		<?php
-		foreach ($rows as $row) {
-			echo '<tr>' . $row . '</tr>';
+		foreach ($rows as $key => $value) {
+			echo '<tr>' . $value . '</tr>';
 		}
 
 		?>
