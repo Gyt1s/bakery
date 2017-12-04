262dc560ba46d1624154d26c3053bfb667b8068a
Gyt1s <33262528+Gyt1s@users.noreply.github.com>
2017-12-04 20:07:52 +0200

    public function

--- app/dto/Bun.php
+++ app/dto/Bun.php
@@ -1,28 +1,17 @@
 <?php
-
 class Bun
 {
-	const OWNER = 'Bakery Inc.';
-
-	public static $OWNER_TWO = 'UAB Kepykla';
-
-	private $a = 'A';
-	protected $b = 'B';
-	public $c = 'C';
+	 public function __construct ()
+	{
+		echo 'hello' . "\r\n";
+	}
 }
 
-Bun::$OWNER_TWO = Bun::OWNER;
-
-
-print_r(Bun::OWNER);
-print_r(Bun::$OWNER_TWO);
-$Bun = new Bun();
-
-
-print_r($Bun);
-print_r($Bun->c);
-
-
+$bun = new Bun ('a');
+$bun = new Bun ('b');
+$bun = new Bun ('c');
+$bun = new Bun ('d');
+$bun = new Bun ('e');
 
 
 
